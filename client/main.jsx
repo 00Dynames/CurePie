f9eee57b4bfbf5418a9eab31b98637d341d8cdf3
Jacqueline <leejjjacq@gmail.com>
2017-09-15 10:37:05 +1000

    Sample buttons included

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import '../imports/startup/client/routes.js';
-import Home from '../imports/ui/Home.jsx';
-import Navbar from '../imports/ui/Navbar.jsx';
+//import '../imports/startup/client/routes.js';
+//import Home from '../imports/ui/Home.jsx';
+// import Navbar from '../imports/ui/Navbar.jsx';
+import App from '../imports/ui/sample.jsx';
 
 Meteor.startup(() => {
-    render(<Navbar />, document.getElementById('navbar-target'));
-    render(<Home />, document.getElementById('home-target'));
+    // render(<Navbar />, document.getElementById('navbar-target'));
+    // render(<Home />, document.getElementById('home-target'));
+    render(<App />, document.getElementById('navbar-target'));
 });
