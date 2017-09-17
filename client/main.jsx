cad57fb2202b7387d5f00e78ae3b3311ad0dc8c7
mahikasuri <moon10@y7mail.com>
2017-09-17 20:03:42 +1000

    Information panel for event details page
    To view the page, render the Event details page in main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -9,9 +9,12 @@ import Navbar from '../imports/ui/Navbar.jsx';
 import Map from '../imports/ui/Map.jsx';
 import Main from '../imports/ui/Main.jsx';
 import Landing from '../imports/ui/landingpg.jsx'
-
-
+import EventDetails from '../imports/ui/event_details.jsx'
 
 Meteor.startup(() => {
     render(<Landing />, document.getElementById('landing-target'));
-});
+})
+
+/*Meteor.startup(() => {
+    render(<EventDetails />, document.getElementById('navbar-target'));
+})*/
