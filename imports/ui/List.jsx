531fde6b1241ee08a197c3f69009fa174adf1cef
Jacqueline <leejjjacq@gmail.com>
2017-09-22 09:06:02 +1000

    With event listing

--- imports/ui/List.jsx
+++ imports/ui/List.jsx
@@ -2,9 +2,14 @@ import React, { Component, PropTypes } from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import SearchBar from 'material-ui-search-bar'
+import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
-export default class List extends Component{
+import '../css/listView.css'
 
+class List extends Component {
     render(){
         return(
             <div>
@@ -21,3 +26,62 @@ export default class List extends Component{
     }
 }
 
+class SearchTag extends Component {
+  render() {
+    return (
+      <div id="search-bar-container">
+        <MuiThemeProvider>
+          <SearchBar
+            onChange={() => console.log('onChange')}
+            onRequestSearch={() => console.log('onRequestSearch')}
+            hintText="SEARCH"
+            style={{
+              margin: '0 auto',
+              maxWidth: 280,
+            }}
+          />
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
+
+class Tags extends Component {
+  render () {
+    return (
+      <div>
+        Or Click on one of the tags below:
+        <div className="tags">
+          #hurricane<br />
+          #burma<br />
+          #landslide<br />
+          #poverty<br />
+          #children<br />
+        </div>
+      </div>
+    )
+  }
+}
+
+class sortDropdown extends Component {
+  render () {
+    return (
+    )
+  }
+}
+
+
+class LeftSideBar extends Component {
+  render() {
+    return (
+      <div>
+        <SearchTag />
+        <Tags />
+        <DropDownMenuSimpleExample />
+      </div>
+    )
+  }
+}
+
+
+export default LeftSideBar;
